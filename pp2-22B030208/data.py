fc3e14059b4518c27498da78f1ad0ecfe4dc9b5b
Madi Yermukhamet <123309763+pp2-22B030208@users.noreply.github.com>
2023-03-07 14:51:12 +0600

    Create data.py

---
+++ pp2-22B030208/data.py
@@ -0,0 +1,29 @@
+'''#1
+from datetime import datetime,date, timedelta
+print(datetime.now()+timedelta(days=-5))'''
+
+
+'''#2
+
+from datetime import datetime,date, timedelta
+print(datetime.now()-timedelta(days=1))
+print(datetime.now())
+print(datetime.now()+timedelta(days=1))'''
+
+'''#3
+import datetime
+from datetime import datetime,date,timedelta
+
+def drop():
+    print(datetime.now().replace(microsecond=0))
+
+drop() '''
+
+'''#4
+from datetime import date, datetime,timedelta,time
+a = datetime.today()
+b = datetime.strptime("2 March 2022, 07:00:00", "%d %B %Y, %H:%M:%S")
+c = (a-b).seconds
+print(datetime.today())
+print(c) '''
+
