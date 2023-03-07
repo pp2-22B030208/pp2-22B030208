2d4836e51b2edfa99d9633df4d9f4a61ac6b7b09
Madi Yermukhamet <123309763+pp2-22B030208@users.noreply.github.com>
2023-03-07 16:14:57 +0600

    Create math.py

---
+++ pp2-22B030208/math.py
@@ -0,0 +1,27 @@
+'''#1
+import math
+from decimal import *
+a=int(input('Input degree: '))
+b=round(a*(math.pi/180),7)
+print(b)
+print('') '''
+''' #2
+
+import math
+a=int(input('Height: '))
+b=int(input('Base, first value: '))
+c=int(input('Base, second value: '))
+print(((b+c)*a)/2) '''
+''' #3
+
+import math
+a=int(input('Input number of sides: '))
+b=int(input('Input the length of a side: '))
+print('The area of the polygon is: ',int((pow(b,2)*a)/(4*math.tan((math.pi/a)))),sep='') '''
+
+'''#4
+a=int(input('Length of base: '))
+b=int(input('Height of parallelogram: '))
+print(float(a*b))
+14:42
+'''
